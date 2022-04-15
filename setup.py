1eb1015aaa1e13827358310de2e4f63524c485ca
Changyu L <liu.changyu@northeastern.edu>
2022-04-14 23:45:04 -0400

    master

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ setup(
     name="Darray",
     version="1.0",
     author="Changyu Liu, Zihan Zhao, Yishui Tian, Jiaran Kang, Jie Ji",
-    author_email="liu.changyu@northeastern.edu, zhao.zih@northeastern.edu, tian.yis@northeastern.edu, kang.jiar@northeastern.edu",
+    author_email="liu.changyu@northeastern.edu, zhao.zih@northeastern.edu, tian.yis@northeastern.edu, kang.jiar@northeastern.edu, ji.jie3@northeastern.edu",
     description="The Darray is a 2-dimensional data structure which is used for data preprocessing",
     url="https://github.com/ChangyuLNeu/Darray",
     license='MIT',
     python_requires='>=3',
-    setup_requires  = ['math', 'numpy', 'io', 'numbers', 'random'],
+    #setup_requires  = ['math', 'numpy', 'io', 'numbers', 'random'],
 )
